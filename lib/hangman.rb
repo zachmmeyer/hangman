371c4fe6d459944735b65d0135b0e25ea4eda06b
Zach Meyer <zachary.michael.meyer@gmail.com>
2021-12-17 16:28:09 -0500

    Add workable inputs to main menu

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -3,9 +3,42 @@
 module Hangman
   # Game class
   class Game
+    def exit_game
+      puts 'Good-bye!'
+      exit
+    end
+
+    def load_game
+      puts 'Load Game'
+    end
+
+    def new_game
+      puts 'New Game'
+    end
+
+    def choice_case(player_choice)
+      case player_choice
+      when '1'
+        new_game
+      when '2'
+        load_game
+      when '3'
+        exit_game
+      else
+        start_game
+      end
+    end
+
+    def choose_game_mode
+      player_choice = gets.chomp
+      choice_case(player_choice)
+    end
+
     def start_game
+      system('clear')
       puts 'Welcome to Hangman!'
       puts 'Press 1 to start a new game of Hangman, Press 2 to load a saved game, or Press 3 to quit'
+      choose_game_mode
     end
   end
 end
