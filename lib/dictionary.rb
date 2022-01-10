9c4f92868cdef699d36450eb62f0cf2b62a5daeb
Zach Meyer <zachary.michael.meyer@gmail.com>
2022-01-10 16:11:39 -0500

    Store secret word

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -2,8 +2,11 @@
 
 # Dictionary class
 class Dictionary
+  attr_accessor :secret
+
   def initialize
     list = generate_dictionary_array('./dictionary.txt')
+    @secret = choose_secret_word(list)
   end
 
   def generate_dictionary_array(dictionary)
