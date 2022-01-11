c33842c637fc1a2e70b2b01ce5e1735d9ad47de0
Zach Meyer <zachary.michael.meyer@gmail.com>
2022-01-11 16:36:19 -0500

    Change secret to secret word and downcase

--- lib/computer.rb
+++ lib/computer.rb
@@ -2,7 +2,11 @@
 
 # Computer class
 class Computer
-  attr_accessor :secret
+  attr_accessor :secret_word
+
+  def generate_new_secret_word
+    @secret_word = File.readlines('./dictionary.txt').sample.downcase
+  end
 
   def initialize
     @secret = File.readlines('./dictionary.txt').sample
