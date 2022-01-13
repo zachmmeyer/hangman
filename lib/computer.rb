8218e3ef71a8e1a7fc77226cb6f3db8a4460e5a3
Zach Meyer <zachary.michael.meyer@gmail.com>
2022-01-13 01:34:24 -0500

    Make secret word length between 5 & 12 characters

--- lib/computer.rb
+++ lib/computer.rb
@@ -6,6 +6,7 @@ class Computer
 
   def generate_new_secret_word
     @secret_word = File.readlines('./dictionary.txt').sample.downcase.chomp
+    generate_new_secret_word unless (5..12).include?(@secret_word.size)
   end
 
   def load_saved_secret_word(loaded_word)
